1b5187ad60e67045d747203f7a1bf18b69829138
Christopher Bull <caffeinatedandroid@gmail.com>
2024-04-27 00:22:05 +0100

    chore: add semantic-release

---
+++ .releaserc.mjs
@@ -0,0 +1,20 @@
+export default {
+  "branches": ["main"],
+  "plugins": [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "semantic-release-chrome",
+      {
+        "extensionId": `${process.env.EXTENSION_ID}`,
+        "asset": "clear-links-extension.zip"
+      }
+    ],
+    [
+      "@semantic-release/github",
+      {
+        "assets": ["clear-links-extension.zip"]
+      }
+    ]
+  ]
+}
