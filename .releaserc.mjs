f972a3d9f7734efd60fbccfb08cd78b8e1378551
Christopher Bull <caffeinatedandroid@gmail.com>
2024-09-07 17:23:10 +0100

    ci: automatically publish firefox extension

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -7,14 +7,26 @@ export default {
       'semantic-release-chrome',
       {
         extensionId: `${process.env.EXTENSION_ID}`,
-        asset: 'clear-links-extension.zip',
+        asset: 'clear-links-extension-chrome.zip',
         distFolder: 'dist/chrome',
+      },
+    ],
+    [
+      'semantic-release-amo',
+      {
+        addonId: '{606f9f3d-1c71-463d-a1aa-d10cf1e4aa64}',
+        addonDirPath: 'dist/firefox',
+        addonZipPath: './clear-links-extension-firefox.zip',
+        submitReleaseNotes: true,
       },
     ],
     [
       '@semantic-release/github',
       {
-        assets: [ 'clear-links-extension.zip' ],
+        assets: [
+          'clear-links-extension-chrome.zip',
+          'clear-links-extension-firefox.zip',
+        ],
       },
     ],
   ],
