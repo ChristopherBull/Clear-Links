10bc098623947ed954ec8626de79d2f844b12b5c
Christopher Bull <caffeinatedandroid@gmail.com>
2026-01-21 17:45:46 +0000

    test: add action badge tests for setErrorStatus and clearStatus

--- test/e2e/popup.test.js
+++ test/e2e/popup.test.js
@@ -7,4 +7,36 @@ test.describe('Action popup', () => {
     await page.goto(`${browserExtensionUrlProtocol}://${extensionId}/action-popup.html`);
     await expect(page).toHaveTitle('Action Popup | Clear Links');
   });
+
+  // ------------ //
+  // Action Badge //
+  // ------------ //
+
+  test('action badge setErrorStatus sets badge text', async ({ backgroundPage }) => {
+    await backgroundPage.evaluate(() => {
+      // eslint-disable-next-line no-undef
+      return ActionBadge.setErrorStatus();
+    });
+    const badgeText = await backgroundPage.evaluate(() => {
+      return browser.action.getBadgeText({});
+    });
+    await expect(badgeText).not.toEqual('');
+  });
+
+  test('action badge clearStatus removes badge text', async ({ backgroundPage }) => {
+    // First set error status
+    await backgroundPage.evaluate(() => {
+      // eslint-disable-next-line no-undef
+      return ActionBadge.setErrorStatus();
+    });
+    // Then clear it
+    await backgroundPage.evaluate(() => {
+      // eslint-disable-next-line no-undef
+      return ActionBadge.clearStatus();
+    });
+    const badgeText = await backgroundPage.evaluate(() => {
+      return browser.action.getBadgeText({});
+    });
+    await expect(badgeText).toEqual('');
+  });
 });
