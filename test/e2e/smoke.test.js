1ea15c1050510e711d45ce71d7ac4250f1cbfc33
Christopher Bull <caffeinatedandroid@gmail.com>
2026-01-23 15:23:43 +0000

    test:  add build integrity and basic service worker check to smoke tests

--- test/e2e/smoke.test.js
+++ test/e2e/smoke.test.js
@@ -1,9 +1,27 @@
 import { expect, test } from './fixtures/fixtures.js';
 
 test.describe('Smoke Tests', () => {
-  // Playwright does not yet support service workers or background pages in
-  // non-chromium browsers (e.g. Firefox). Skips this whole group accordingly.
-  test.skip(({ browserName }) => browserName !== 'chromium', 'Chromium only! Testing with Service Workers or Background Pages in Playwright is not yet possible in non-chromium browsers.');
+  test('build integrity: manifest.json is bundled', async ({ optionsPage }) => {
+    const exists = await optionsPage.evaluate(async () => {
+      const url = chrome.runtime.getURL('manifest.json');
+      const response = await fetch(url);
+      return response.ok;
+    });
+    expect(exists).toBe(true);
+  });
+
+  test('Service Workers currently only expected in Chromium test runs', async ({ browserName, context }) => {
+    // Some tests are skipped due to Playwright not yet supporting service workers in non-chromium browsers.
+    // If this tests fails, it means that Playwright now supports service workers in non-chromium browsers and the skipped tests should be tested and re-enabled.
+    // NB: https://playwright.dev/docs/api/class-browsercontext#browser-context-service-workers
+    if (browserName === 'chromium') {
+      // Expect service workers to be a function in chromium browsers
+      await expect(context.serviceWorkers).toBeDefined();
+    } else {
+      // Expect service workers to be undefined in non-chromium browsers
+      await expect(context.serviceWorkers).toBeUndefined();
+    }
+  });
 
   test('isTestBuild flag must exist to ensure running against test build', async ({ backgroundPage }) => {
     // Fail fast if running against a production build instead of a test build
